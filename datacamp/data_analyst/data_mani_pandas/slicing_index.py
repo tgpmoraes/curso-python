a010a6d19fdbebaadd3b7cae934451d5b2b53402
tgpmoraes <tgpmoraes@gmail.com>
2021-10-06 08:43:49 +0000

    more slicing

--- datacamp/data_analyst/data_mani_pandas/slicing_index.py
+++ datacamp/data_analyst/data_mani_pandas/slicing_index.py
@@ -71,5 +71,28 @@ temp_by_country_city_vs_year = temperatures.pivot_table(
     index=['Country', 'City'],
     columns='year')
 
-# See the result
-print(temp_by_country_city_vs_year)
+# Subset for Egypt to India
+print(temp_by_country_city_vs_year.loc['Egypt':'India'])
+
+# Subset for Egypt, Cairo to India, Delhi
+print(temp_by_country_city_vs_year.loc[('Egypt', 'Cairo'):('India', 'Delhi')])
+
+# Subset in both directions at once
+print(temp_by_country_city_vs_year.loc[
+    ('Egypt', 'Cairo'):('India', 'Delhi'), 2005:2010])
+
+# Get the worldwide mean temp by year
+mean_temp_by_year = temp_by_country_city_vs_year.mean(axis='index')
+
+# Filter for the year that had the highest mean temp
+print(mean_temp_by_year[
+    mean_temp_by_year == temp_by_country_city_vs_year
+    .mean(axis='index').max()])
+
+# Get the mean temp by city
+mean_temp_by_city = temp_by_country_city_vs_year.mean(axis='columns')
+
+# Filter for the city that had the lowest mean temp
+print(mean_temp_by_city[
+    mean_temp_by_city == temp_by_country_city_vs_year
+    .mean(axis='columns').min()])
