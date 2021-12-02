96a6d8ac61b42268a8bd57f7e579bbe5ec32ef18
tgpmoraes <tgpmoraes@gmail.com>
2021-12-02 09:05:00 +0000

    Writing your own functions

---
+++ datacamp/python_programer/ds_toolbox/import_tweets_df.py
@@ -0,0 +1,30 @@
+# Import pandas
+import os
+import pandas as pd
+
+
+curso_dir = 'C:\\Users\\tiagog\\Documents\\curso-python'
+data_dir = curso_dir + '\\datacamp\\python_programer\\ds_toolbox'
+os.chdir(data_dir)
+
+# Import Twitter data as DataFrame: df
+df = pd.read_csv('tweets.csv')
+
+# Initialize an empty dictionary: langs_count
+langs_count = {}
+
+# Extract column from DataFrame: col
+col = df['lang']
+
+# Iterate over lang column in DataFrame
+for entry in col:
+
+    # If the language is in langs_count, add 1
+    if entry in langs_count.keys():
+        langs_count[entry] += 1
+    # Else add the language to langs_count, set the value to 1
+    else:
+        langs_count[entry] = 1
+
+# Print the populated dictionary
+print(langs_count)
