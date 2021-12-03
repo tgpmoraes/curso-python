6ba7fb4174d2530474e2e393a2545baa181b9071
tgpmoraes <tgpmoraes@gmail.com>
2021-12-03 12:53:11 +0000

    Lambda functions and error-handling

---
+++ datacamp/python_programer/ds_toolbox/import_tweets_df_v5.py
@@ -0,0 +1,59 @@
+# Import pandas
+import os
+import pandas as pd
+
+
+curso_dir = 'C:\\Users\\tiagog\\Documents\\curso-python'
+data_dir = curso_dir + '\\datacamp\\python_programer\\ds_toolbox'
+os.chdir(data_dir)
+
+# Import Twitter data as DataFrame: df
+tweets_df = pd.read_csv('tweets.csv')
+
+
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+
+    # Add try block
+    try:
+        # Extract column from DataFrame: col
+        col = df[col_name]
+
+        # Iterate over the column in dataframe
+        for entry in col:
+
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
+
+        # Return the cols_count dictionary
+        return cols_count
+
+    # Add except block
+    except:
+        print('The DataFrame does not have a ' + col_name + ' column.')
+
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
+
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'lang', 'source')
+
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda text: text[:2] == 'RT', tweets_df['text'])
+
+# Create list from filter object result: res_list
+res_list = list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
