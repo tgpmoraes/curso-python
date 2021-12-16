5a42a02d36ee62adf7da5997b3146cc55358ecf8
tgpmoraes <tgpmoraes@gmail.com>
2021-12-16 12:20:06 +0000

    Formatting Strings

---
+++ datacamp/python_programer/regular_expression/format_string.py
@@ -0,0 +1,14 @@
+field1 = 'sexiest job'
+field2 = 'data is produced daily'
+field3 = 'Individuals'
+fact1 = 21
+fact2 = 2500000000000000000
+fact3 = 72.41415415151
+fact4 = 1.09
+
+print(f"Data science is considered {field1!r} in the {fact1:d}st century")
+
+print(f"About {fact2:e} of {field2} in the world")
+
+print(f"{field3} create around {fact3:.2f}% of the data but \
+only {fact4:.1f}% is analyzed")
