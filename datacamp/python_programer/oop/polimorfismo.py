44f14ebfd3a544a2457ab2ba654ada4b18498303
Tiago Moraes <tgpmoraes@gmail.com>
2022-02-23 11:44:48 +0000

    Best Practices of Class Design

--- datacamp/python_programer/oop/polimorfismo.py
+++ datacamp/python_programer/oop/polimorfismo.py
@@ -1,22 +1,24 @@
-# Import pandas as pd
 import pandas as pd
 from datetime import datetime
 
-# Define LoggedDF inherited from pd.DataFrame and add the constructor
+# MODIFY the class to use _created_at instead of created_at
 
 
 class LoggedDF(pd.DataFrame):
-
     def __init__(self, *args, **kwargs):
         pd.DataFrame.__init__(self, *args, **kwargs)
-        self.created_at = datetime.today()
+        self._created_at = datetime.today()
 
     def to_csv(self, *args, **kwargs):
-        # Copy self to a temporary DataFrame
         temp = self.copy()
-
-        # Create a new column filled with self.created_at
-        temp["created_at"] = self.created_at
-
-        # Call pd.DataFrame.to_csv on temp, passing in *args and **kwargs
+        temp["created_at"] = self._created_at
         pd.DataFrame.to_csv(temp, *args, **kwargs)
+
+    # Add a read-only property: _created_at
+    @property
+    def created_at(self):
+        return self._created_at
+
+
+# Instantiate a LoggedDF called ldf
+ldf = LoggedDF({"col1": [1, 2], "col2": [3, 4]})
